a546d7a2ae86bc35440478a86f3e0009e2896463
Miguel <Miguel@172.19.186.220>
2018-03-12 09:56:33 +0100

    Part A2 finished

--- A2/Signalled_Queue.java
+++ A2/Signalled_Queue.java
@@ -1,6 +1,8 @@
 package concurrent_assignment2.A2;
 
 import concurrent_assignment2.A_intro.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Use the synchronized keyword and signals so that
@@ -11,19 +13,40 @@ import concurrent_assignment2.A_intro.Queue;
  */
  
 class Signalled_Queue implements Queue{
-	int n=0;
 	
-	@Override
-	public void read() {
-		// TODO Auto-generated method stub
-		
-	}
+    volatile int n = 0;
+    volatile boolean cond=false;
 
-	@Override
-	public void write(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    synchronized public void read() {
+        if(!cond){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Signalled_Queue.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println(n);
+        cond=false;
+        notify();
+    }
+
+    @Override
+    synchronized public void write(int x) {
+            // TODO Auto-generated method stub
+            if(cond){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Signalled_Queue.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            n++;
+            System.out.println(n);
+            cond=true;
+            notify();
+            
+    }
 
 	@Override
 	public void read(int ID) {
